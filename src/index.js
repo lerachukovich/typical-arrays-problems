fec640bbeecd25b5832ff5f9e3c083eac1d018b0
Lera Chukovich <chukalera@gmail.com>
2020-09-29 12:57:48 +0200

    feat: add problem solving of typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array == null || array.length == 0) {
+        return 0;
+      }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array == null || array.length == 0) {
+        return 0;
+      }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array == null || array.length == 0) {
+        return 0;
+    }
+    let sumOfArray = array.reduce((a,b) => a + b);
+    let averageNumber = sumOfArray / array.length;
+    return averageNumber;
 }
+
+
+
